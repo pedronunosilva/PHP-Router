55ee1bece70e3f5c7b5643719a3fefbd24c0bf55
Pedro Nuno Silva <pedrosva@dakar.lan>
2015-10-28 22:20:02 +0000

    static pages

---
+++ pages/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="pt-pt">
+
+<head>
+
+	<meta charset="UTF-8">
+	
+	<title>Page not found</title>
+
+</head>
+
+<body>
+	
+	<h1>Page not found</h1>
+
+</body>
+
+</html>
---
+++ pages/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="pt-pt">
+
+<head>
+
+	<meta charset="UTF-8">
+	
+	<title>Index</title>
+
+</head>
+
+<body>
+	
+	<h1>Hello</h1>
+
+</body>
+
+</html>
